7bbedcd9815e37aaecc3babf28024ff41c7f3305
Neutrino-Go <101853167+Neutrino-Go@users.noreply.github.com>
2022-03-23 21:15:07 +0800

    转义字符

---
+++ test_3_23/转义字符.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	printf("c:\test\test.c\n");
+	printf("ab\ncd\n");
+	printf("%c\n", '\'');
+	printf("%s\n", "abc");
+	printf("%s\n", "\"");
+
+	return 0;
+}
