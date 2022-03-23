0f31c8794a470276a79a15f93312dfe1f254d65f
Neutrino-Go <101853167+Neutrino-Go@users.noreply.github.com>
2022-03-23 18:12:10 +0800

    字符串+枚举常量

---
+++ test_3_23/字符串.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//×Ö·û´®
+#include<stdio.h>
+int main(void)
+{
+	char arr1[] = "abc";
+	char arr2[] = { 'a','b','c' };
+
+	//´òÓ¡×Ö·û´®
+	printf("%s\n", arr1);
+	printf("%s\n", arr2);
+
+	return 0;
+}
---
+++ test_3_23/枚举常量.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//Ã¶¾Ù³£Á¿
+#include<stdio.h>
+enum SEX
+{
+	Male,
+	Female,
+	Secret
+};
+
+int main(void)
+{
+	printf("%d\n%d\n%d\n", Male, Female, Secret);
+	return 0;
+}
