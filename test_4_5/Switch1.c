fbc1ee26647ea57969efeb49c435492be4f8eb61
Neutrino-Go <101853167+Neutrino-Go@users.noreply.github.com>
2022-04-05 21:15:54 +0800

    Switch

---
+++ test_4_5/Switch1.c
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int n = 1;
+	int m = 2;
+	switch (n)
+	{
+	case 1:
+		m++; //n=1,m=3
+	case 2:
+		n++; //n=2,m=3
+	case 3:
+		switch (n)
+		{
+		case 1:
+			n++;
+		case 2:
+			m++;
+			n++; //n=3,m=4
+			break;
+		}
+	case 4:
+		m++; //n=3,m=5
+		break;
+	default:
+		break;
+	}
+	printf("m = %d, n = %d\n", m, n);
+	return 0;
+}
