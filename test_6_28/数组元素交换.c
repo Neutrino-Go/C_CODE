bb409803ba68e1aa603580fb307c237fe53c6b0d
Neutrino-Go <101853167+Neutrino-Go@users.noreply.github.com>
2022-06-30 10:09:31 +0800

    数组元素交换

---
+++ test_6_28/数组元素交换.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int arr1[] = { 1,3,5,7,9 };
+	int arr2[] = { 2,4,6,8,10 };
+
+	int sz = sizeof(arr1) / sizeof(arr1[0]);
+	int i = 0;
+
+	for (i = 0; i < sz; i++)
+	{
+		int tmp = arr1[i];
+		arr1[i] = arr2[i];
+		arr2[i] = tmp;
+	}
+
+	printf("arr1: ");
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr1[i]);
+	}
+	printf("\n");
+
+	printf("arr2: ");
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr2[i]);
+	}
+	printf("\n");
+
+	return 0;
+}
